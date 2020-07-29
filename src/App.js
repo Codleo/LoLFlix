908d5d5eef6501c12590d3f2cff24421aeabb3f9
Codleo <leo.santos2001@hotmail.com>
2020-07-29 05:25:12 -0300

    master

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Menu from './components/Menu'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Menu></Menu>
     </div>
   );
 }
-
+//Update automático (Hot Reaload)
+//JSX -> JAVA SCRIPT XML
 export default App;
